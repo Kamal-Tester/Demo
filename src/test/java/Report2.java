e153d77ca11b01eedadf0d1557595ae0929a91c8
Kamal-Tester <meghani58155@gmail.com>
2024-10-10 20:47:36 +0530

    demo

---
+++ src/test/java/Report2.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Report2 {
+
+	@Test
+	public void main() {
+		System.out.print("Not yet implemented");
+	}
+
+}
---
+++ src/test/java/TestRunner/OdyRunner.java
@@ -0,0 +1,13 @@
+package TestRunner;
+
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+	@CucumberOptions(features="src\\test\\resources\\Feature\\Odyssey.feature",
+	glue= {"StepDefination"},
+	plugin= {"pretty","html:target/htmlreports.html"},
+	monochrome = true)
+	public class OdyRunner extends AbstractTestNGCucumberTests{
+
+}
